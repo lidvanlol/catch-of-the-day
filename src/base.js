9a993a3a2db9e5f1f67f20eddbb27d21163a3813
lidvanlol <lidvanlol@gmail.com>
2018-01-17 15:17:01 +0100

    Start with create-react-app

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+    apiKey: "AIzaSyDCQge-rut61wez8X5xTYy12CXcp4d6BkU",
+    authDomain: "catch-of-the-day-aa239.firebaseapp.com",
+    databaseURL: "https://catch-of-the-day-aa239.firebaseio.com"
+});
+
+export default base;
